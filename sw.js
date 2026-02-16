98e7c1075addf6f6ddf75a0f321fef1d590a0c6a
DiamTech <116391367+DiamTech@users.noreply.github.com>
2026-02-16 18:20:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
+const CACHE_NAME = 'applifilm-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  'https://unpkg.com/lucide@latest',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.23/jspdf.plugin.autotable.min.js'
+];
+
+// Installation : on met les fichiers en cache
 self.addEventListener('install', (e) => {
-  console.log('Service Worker installé');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
+// Stratégie : Réseau d'abord, sinon Cache (pour être tjs à jour si on a de la data)
 self.addEventListener('fetch', (e) => {
-  // Indispensable pour le mode hors-ligne plus tard
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
